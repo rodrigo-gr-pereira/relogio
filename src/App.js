508054a1793c643866bf42b9cc9ba54a1cddfb52
Rodrigo das Gracas Pereira <rodrigogr.pereira@gmail.com>
2024-05-16 20:54:16 -0300

    relogio-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  
+  const [hora, setHora] = useState(19);
+  const [minuto, setMinuto] = useState(55);
+  const [segundo,setSegundo] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+      const interval = setInterval(( )=>{
+        setSegundo(segundo+1);
+        if(segundo === 59){
+          setSegundo(0);
+          setMinuto(minuto+1);
+          if(minuto === 60){
+            setMinuto(0);
+            setHora(hora+1);
+          }
+        }
+      }, 1000);
+      return() => clearInterval(interval);
+    });
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 style={{textAlign:'center'}}>{hora}: {minuto}: {segundo}</h2>
     </div>
   );
 }
-
-export default App;
